8ed5723107de39eb759a723f570bc78cd79aa3d4
hado0123 <hado_123@naver.com>
2024-11-19 18:32:31 +0900

    up

--- ch03/03_cookie_session/cookie.js
+++ ch03/03_cookie_session/cookie.js
@@ -5,12 +5,13 @@ require('dotenv').config() // env파일을 사용하기 위한 라이브러리
 const app = express()
 app.set('port', process.env.PORT || 3000)
 
-// 쿠키 파서 설정 (서명 키 포함)
+// 쿠키 파서 설정 (보안을 강화하기 위해 서명을 추가한 쿠키)
 app.use(cookieParser('my-secret-key'))
 
 // 쿠키 설정
 app.get('/set-cookie', (req, res) => {
-   res.cookie('user', 'Alice', { signed: true, maxAge: 3600000 }) // 1시간 동안 유효
+   res.cookie('age', '25', { signed: false, maxAge: 3600000 }) // 1시간 동안 유효(서명 보안 X)
+   res.cookie('user', 'Alice', { signed: true, maxAge: 3600000 }) // 1시간 동안 유효(서명 보안 됨)
    res.send('서명된 쿠키가 설정되었습니다.')
 })
 
@@ -18,11 +19,12 @@ app.get('/set-cookie', (req, res) => {
 app.get('/get-cookie', (req, res) => {
    console.log('Cookies:', req.cookies) // 일반 쿠키
    console.log('Signed Cookies:', req.signedCookies) // 서명된 쿠키
-   res.send(`쿠키: ${req.cookies.user}, 서명된 쿠키: ${req.signedCookies.user}`)
+   res.send(`쿠키: ${req.cookies.age}, 서명된 쿠키: ${req.signedCookies.user}`)
 })
 
 // 쿠키 삭제
 app.get('/clear-cookie', (req, res) => {
+   res.clearCookie('age')
    res.clearCookie('user')
    res.send('쿠키가 삭제되었습니다!')
 })
