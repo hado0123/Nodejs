703a4af952c69ed9bef7de374ea6627913f10dfb
hado0123 <hado_123@naver.com>
2024-11-19 16:18:33 +0900

    up

--- ch02/03_1_innerModule/crypto/hash.js
+++ ch02/03_1_innerModule/crypto/hash.js
@@ -1,3 +1,6 @@
+//단방향 암호화: 복호화(암호화 한것을 원래대로 되돌려 놓음) 할 수 없는 암호화 방식
+//고객의 비밀번호를 암호화 할때 사용
+
 const crypto = require('crypto')
 
 console.log('base64:', crypto.createHash('sha512').update('비밀번호').digest('base64'))
