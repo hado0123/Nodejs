75af9991fa5a9d3571b5841bf0190dbe73a4e79f
hado0123 <hado_123@naver.com>
2024-11-21 18:33:52 +0900

    up

--- ch02/04_1_FileSystem/readFile.js
+++ ch02/04_1_FileSystem/readFile.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require('fs')
 
 fs.readFile('./readme.txt', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log(data);
-  console.log(data.toString());
-});
+   if (err) {
+      throw err
+   }
+   console.log(data)
+   console.log(data.toString())
+})
--- ch02/04_1_FileSystem/writeFile.js
+++ ch02/04_1_FileSystem/writeFile.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
+const fs = require('fs')
 
 fs.writeFile('./writeme.txt', '글이 입력됩니다', (err) => {
-  if (err) {
-    throw err;
-  }
-  fs.readFile('./writeme.txt', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log(data.toString());
-  });
-});
+   if (err) {
+      throw err
+   }
+   fs.readFile('./writeme.txt', (err, data) => {
+      if (err) {
+         throw err
+      }
+      console.log(data.toString())
+   })
+})
